6154fd3776871151b771ee151bd48c59a493f903
lhade <105942600+lhade@users.noreply.github.com>
2022-10-05 12:18:47 +0200

    loginscreen

---
+++ auth.js
@@ -0,0 +1,35 @@
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "./app";
+//import { auth } from "@react-native-firebase/auth";
+
+//const auth = getAuth(app);
+
+export function connectUser(email, password) {
+    return new Promise((res, rej) => {
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                res(userCredential.user);
+                // ...
+            })
+            .catch((error) => {
+                rej(error.message);
+            });
+    })
+}
+
+export function createUser(email, password) {
+    return new Promise((res, rej) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                res(userCredential.user);
+                // ...
+            })
+            .catch((error) => {
+                rej(error.message);
+                // ..
+            });
+    })
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen =() => {
+    return (
+        <View>
+            <Text>
+                bonjour je suis le home screen
+            </Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
